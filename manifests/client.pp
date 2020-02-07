830df6a5a16ced3956eb88ced28ae08fa4493bae
Erik Geiger <erik@frontify.com>
2020-02-07 18:41:01 +0100

    fixing wrong path for exec command

--- manifests/client.pp
+++ manifests/client.pp
@@ -103,7 +103,7 @@ class bacula::client (
     exec { "create $pki_keypair_path":
       creates => $pki_keypair_path,
       command => "/bin/mkdir -p $pki_keypair_path",
-      path    => 'bin:/usr/local/bin:/usr/bin',
+      path    => '/bin:/usr/local/bin:/usr/bin',
       require => Package[$client_package],
       before  => File[$pki_keypair_path],
     }
@@ -118,7 +118,7 @@ class bacula::client (
       command => "openssl genrsa -out /tmp/private.key 4096 && openssl req -new -key /tmp/private.key -x509 -out /tmp/public.crt -subj '/C=XX/ST=unknown/L=puppet-bacula/O=Bacula Backup/OU=backup/CN=${::fqdn}' && cat /tmp/private.key /tmp/public.crt >$pki_keypair && chmod 0400 $pki_keypair && rm /tmp/private.key /tmp/public.crt",
       creates => "$pki_keypair",
       notify  => Service['bacula-fd'],
-      path    => 'bin:/usr/local/bin:/usr/bin',
+      path    => '/bin:/usr/local/bin:/usr/bin',
       user    => 'bacula',
       require   => Package[$client_package],
     }
